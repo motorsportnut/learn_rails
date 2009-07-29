393cfd040e2dcf938305090a206567a8a5545f20
motorsportnut <motorsportnut@mac.com>
2009-07-29 10:35:00 -0700

    Initial checkin

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application, "learningrails"
+
+set :deploy_to, "/var/www/apps/#{application}"
+
+role :app, "sampleapp.learningrails.com"
+role :web, "sampleapp.learningrails.com"
+role :db,  "sampleapp.learningrails.com", :primary => true
+
+default_run_options[:pty] = true
+set :repository,  "git://github.com/mzslater/learning-rails-sample-app.git"
+set :scm, "git"
+set :user, "deploy"
+#set :use_sudo, false
+set :admin_runner, "deploy"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  desc "Start Application -- not needed for Passenger"
+  task :start, :roles => :app do
+    # nothing -- need to override default cap start task when using Passenger
+  end
+end
